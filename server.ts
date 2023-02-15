816fc5d4448b9c772469792ef52102acc74bf56a
leirdan <uouleirdan@gmail.com>
2023-02-15 12:46:16 -0300

    refactor: add url to the code

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import * as http from "http";
+const port = 5454;
+const url = `http://localhost:${port}`;
 
 http.createServer((req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
 	res.writeHead(200, { "Content-type": "text/plain" });
 	res.write("welcome, my son... \n");
 	res.write("welcome to the machine.");
 	res.end();
-}).listen(5454, () => {
-	console.log("server is running!");
+}).listen(port, () => {
+	console.log(`server is running in ${url}`);
 });
