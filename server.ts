7594db0bb9d3a2217b7ea56a010c262b9776ce86
leirdan <uouleirdan@gmail.com>
2023-02-15 12:43:12 -0300

    feat: add some text to browser

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import * as http from "http";
 
-http.createServer().listen(7212, () => {
+http.createServer((req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>) => {
+	res.writeHead(200, { "Content-type": "text/plain" });
+	res.write("welcome, my son... \n");
+	res.write("welcome to the machine.");
+	res.end();
+}).listen(5454, () => {
 	console.log("server is running!");
 });
